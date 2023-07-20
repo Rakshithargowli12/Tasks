859b39e64ffe6bacea92b280c7f8e2abe554125f
Rakshithargowli12 <rakshithargowli.xworkz@gmail.com>
2023-07-20 13:37:05 +0530

    messade

---
+++ Self1.java
@@ -0,0 +1,12 @@
+class Self1{
+	 public static void main(String[] args)
+	 {
+		 System.out.println("my name is rakshitha");
+		  System.out.println("i am native to davangere");
+		   System.out.println("i completed my be in ec at ubdt college in 2023 with 8.14");
+		    System.out.println("my hobbies consists of watching dramas");
+			 System.out.println("i am a quick learner");
+		 
+		 
+	 }
+}
