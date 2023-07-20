e0606c1bf4e304059b0c6527626f87263b817863
Rakshithargowli12 <rakshithargowli.xworkz@gmail.com>
2023-07-20 12:18:32 +0530

    java codes

---
+++ Friends.java
@@ -0,0 +1,10 @@
+class Friends{
+	 public static void main(String[] args)
+	 {
+		 
+		System.out.println("i have so friends in my life");
+		  System.out.println("i choose fiends by quality not quantity");
+		   System.out.println("i make friends in each stage of my life");
+		    System.out.println("those idiots made my school days and college days nostalgic");
+	 }
+}
---
+++ IfE.java
@@ -0,0 +1,9 @@
+class IfE{
+public static void main(String[] things){
+int a=20;
+if (a<15){
+ System.out.println("10 is less than 15");
+}
+System.out.println("outside if-block");
+}
+}
